55850a022f3f49a722d08aadc1bfd2f70b154a17
Ria Mehta <rmehta4407@gmail.com>
2026-02-04 22:57:02 +0530

    Create Sum_Two_Numbers.c

---
+++ Sum_Two_Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    
+    printf("enter the value of a=");
+    scanf("%d", &a);
+    printf("Enter the value of b=");
+    scanf("%d", &b);
+
+    sum = a + b
+    printf("%f", sum);
+    return 0;
+}
